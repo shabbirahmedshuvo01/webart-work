4964609fed24c0be9dea4e9a5bb3aae36e259c2d
unknown <sabbirshuvo006@gmail.com>
2023-05-17 22:19:11 +0600

    almot done

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Cards = () => {
+
+    
+
     return (
         <div>
             <h1 className='text-4xl font-bold'>Pricing Plans</h1>
@@ -38,15 +41,16 @@ const Cards = () => {
                     </div>
                     <button className='btn btn-primary w-56 mt-5'>Add To Cart</button>
                 </div>
-                <div className='border border-primary h-96 w-64 rounded-lg shadow-2xl'>
-                    <div className='bg-primary w-64 h-20 text-white  justify-center rounded-t-lg'>
-                        <h1 className='text-xl pt-5'>Basic package</h1>
-                        <h1 className='text-xl pb-2'>$29 <span className='text-sm'>/Start From</span> </h1>
+                <div className='border border-orange-400 h-5/6 pb-5 mt-5 w-64 rounded-lg shadow-2xl'>
+                    <div className='bg-orange-400 w-64 h-20 text-white  justify-center rounded-t-lg'>
+                        <h1 className='text-xl pt-5'>standard package</h1>
+                        <h1 className='text-xl pb-2'>$99 <span className='text-sm'>/Start From</span> </h1>
                     </div>
                     <div className='mt-5 text-xs flex flex-row justify-center items-center gap-x-3'>
 
-                        <div className='text-primary flex flex-col text-start gap-y-2'>
+                        <div className='text-orange-400 flex flex-col text-start gap-y-2'>
                             <p>✔</p>
+                            <p className='mt-4'>✔</p>
                             <p>✔</p>
                             <p>✔</p>
                             <p>✔</p>
@@ -57,26 +61,31 @@ const Cards = () => {
                         </div>
 
                         <div className='flex flex-col gap-y-2 text-start'>
-                            <p>1 Concept Included</p>
+                            <p>2 concept included with Hi <br /> quality design</p>
                             <p>Logo Transparency</p>
                             <p>Vector File</p>
                             <p>Printable File</p>
                             <p>Include 3D Mockup</p>
                             <p>Include source file</p>
+                            <p>Include social media kit</p>
                             <p>Unlimited Revisions</p>
                             <p>3 Days Delivery</p>
                         </div>
                     </div>
-                    <button className='btn btn-primary w-56 mt-5'>Add To Cart</button>
+                    <button className='btn bg-orange-400 border-none w-56 mt-5'>Add To Cart</button>
                 </div>
-                <div className='border border-primary h-96 w-64 rounded-lg shadow-2xl'>
-                    <div className='bg-primary w-64 h-20 text-white  justify-center rounded-t-lg'>
-                        <h1 className='text-xl pt-5'>Basic package</h1>
-                        <h1 className='text-xl pb-2'>$29 <span className='text-sm'>/Start From</span> </h1>
+
+
+                <div className='border border-red-500 h-5/6 pb-5 mt-10 w-64 rounded-lg shadow-2xl'>
+                    <div className='bg-red-500 w-64 h-20 text-white  justify-center rounded-t-lg'>
+                        <h1 className='text-xl pt-5'>premium package</h1>
+                        <h1 className='text-xl pb-2'>$149 <span className='text-sm'>/Start From</span> </h1>
                     </div>
                     <div className='mt-5 text-xs flex flex-row justify-center items-center gap-x-3'>
 
-                        <div className='text-primary flex flex-col text-start gap-y-2'>
+                        <div className='text-red-500 flex flex-col text-start gap-y-2'>
+                            <p>✔</p>
+                            <p className='mt-4'>✔</p>
                             <p>✔</p>
                             <p>✔</p>
                             <p>✔</p>
@@ -88,17 +97,19 @@ const Cards = () => {
                         </div>
 
                         <div className='flex flex-col gap-y-2 text-start'>
-                            <p>1 Concept Included</p>
+                            <p>3 concept included with <br /> Premium quality design</p>
                             <p>Logo Transparency</p>
                             <p>Vector File</p>
                             <p>Printable File</p>
                             <p>Include 3D Mockup</p>
                             <p>Include source file</p>
+                            <p>Stationery designs</p>
+                            <p>Include social media kit</p>
                             <p>Unlimited Revisions</p>
-                            <p>3 Days Delivery</p>
+                            <p>4 Days Delivery</p>
                         </div>
                     </div>
-                    <button className='btn btn-primary w-56 mt-5'>Add To Cart</button>
+                    <button className='btn bg-red-500 border-none w-56 mt-5'>Add To Cart</button>
                 </div>
             </section>
 
--- src/components/CartPage/CartPage.js
+++ src/components/CartPage/CartPage.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const CartPage = () => {
     return (
-        <div>
-            <h1>this is our cart page</h1>
+        <div className='h-screen m-20'>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title">Card title!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
